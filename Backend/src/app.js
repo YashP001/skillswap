c3eb9314fd04f7665b5b11cbbc24b021d5c0b320
YashP001 <yyashpurwar123@gmail.com>
2026-01-19 22:58:23 +0530

    Fix trust proxy and CORS for Google OAuth

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,25 +4,31 @@ import cookieParser from "cookie-parser";
 import passport from "passport";
 
 const app = express();
+app.set("trust proxy", 1);
+
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://skillswap-rosy.vercel.app"
+    ],
     credentials: true,
   })
 );
+
 
 app.use(express.json({ limit: "16kb" })); // to parse json in body
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); // to parse url
 app.use(express.static("public")); // to use static public folder
 app.use(cookieParser()); // to enable CRUD operation on browser cookies
 
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  // Add other CORS headers as needed
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   // Add other CORS headers as needed
+//   next();
+// });
 
 // Passport middleware
 app.use(passport.initialize());
